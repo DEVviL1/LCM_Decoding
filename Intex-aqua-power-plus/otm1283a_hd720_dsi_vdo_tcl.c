7b6647e09e3097dfa87c7bebbc9b0ad5ec92e34e
Stain995 <stainc995@gmail.com>
2018-02-07 15:02:35 +0000

    update

--- Intex-aqua-power-plus/otm1283a_hd720_dsi_vdo_tcl.c
+++ Intex-aqua-power-plus/otm1283a_hd720_dsi_vdo_tcl.c
@@ -1,13 +1,82 @@
-void lcm_set_util_funcs_(LCM_UTIL_FUNCS *util)
+/*****************************************************************************
+*  Copyright Statement:
+*  --------------------
+*  This software is protected by Copyright and the information contained
+*  herein is confidential. The software may not be copied and the information
+*  contained herein may not be used or disclosed except with the written
+*  permission of MediaTek Inc. (C) 2008
+*
+*  BY OPENING THIS FILE, BUYER HEREBY UNEQUIVOCALLY ACKNOWLEDGES AND AGREES
+*  THAT THE SOFTWARE/FIRMWARE AND ITS DOCUMENTATIONS ("MEDIATEK SOFTWARE")
+*  RECEIVED FROM MEDIATEK AND/OR ITS REPRESENTATIVES ARE PROVIDED TO BUYER ON
+*  AN "AS-IS" BASIS ONLY. MEDIATEK EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES,
+*  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+*  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NONINFRINGEMENT.
+*  NEITHER DOES MEDIATEK PROVIDE ANY WARRANTY WHATSOEVER WITH RESPECT TO THE
+*  SOFTWARE OF ANY THIRD PARTY WHICH MAY BE USED BY, INCORPORATED IN, OR
+*  SUPPLIED WITH THE MEDIATEK SOFTWARE, AND BUYER AGREES TO LOOK ONLY TO SUCH
+*  THIRD PARTY FOR ANY WARRANTY CLAIM RELATING THERETO. MEDIATEK SHALL ALSO
+*  NOT BE RESPONSIBLE FOR ANY MEDIATEK SOFTWARE RELEASES MADE TO BUYER'S
+*  SPECIFICATION OR TO CONFORM TO A PARTICULAR STANDARD OR OPEN FORUM.
+*
+*  BUYER'S SOLE AND EXCLUSIVE REMEDY AND MEDIATEK'S ENTIRE AND CUMULATIVE
+*  LIABILITY WITH RESPECT TO THE MEDIATEK SOFTWARE RELEASED HEREUNDER WILL BE,
+*  AT MEDIATEK'S OPTION, TO REVISE OR REPLACE THE MEDIATEK SOFTWARE AT ISSUE,
+*  OR REFUND ANY SOFTWARE LICENSE FEES OR SERVICE CHARGE PAID BY BUYER TO
+*  MEDIATEK FOR SUCH MEDIATEK SOFTWARE AT ISSUE.
+*
+*  THE TRANSACTION CONTEMPLATED HEREUNDER SHALL BE CONSTRUED IN ACCORDANCE
+*  WITH THE LAWS OF THE STATE OF CALIFORNIA, USA, EXCLUDING ITS CONFLICT OF
+*  LAWS PRINCIPLES.  ANY DISPUTES, CONTROVERSIES OR CLAIMS ARISING THEREOF AND
+*  RELATED THERETO SHALL BE SETTLED BY ARBITRATION IN SAN FRANCISCO, CA, UNDER
+*  THE RULES OF THE INTERNATIONAL CHAMBER OF COMMERCE (ICC).
+*
+*****************************************************************************/
+#ifndef BUILD_LK
+#include <linux/string.h>
+#endif
+#include "lcm_drv.h"
+
+#ifdef BUILD_LK
+	#include <platform/mt_gpio.h>
+#elif defined(BUILD_UBOOT)
+	#include <asm/arch/mt_gpio.h>
+#else
+	#include <mach/mt_gpio.h>
+#endif
+
+// ---------------------------------------------------------------------------
+//  Local Variables
+// ---------------------------------------------------------------------------
+
+static LCM_UTIL_FUNCS lcm_util = {0};
+
+#define SET_RESET_PIN(v)    								(lcm_util.set_reset_pin((v)))
+
+#define UDELAY(n) 											(lcm_util.udelay(n))
+#define MDELAY(n) 											(lcm_util.mdelay(n))
+
+
+// ---------------------------------------------------------------------------
+//  Local Functions
+// ---------------------------------------------------------------------------
+
+#define dsi_set_cmdq_V2(cmd, count, ppara, force_update)	        lcm_util.dsi_set_cmdq_V2(cmd, count, ppara, force_update)
+#define dsi_set_cmdq(pdata, queue_size, force_update)		lcm_util.dsi_set_cmdq(pdata, queue_size, force_update)
+#define wrtie_cmd(cmd)										lcm_util.dsi_write_cmd(cmd)
+#define write_regs(addr, pdata, byte_nums)					lcm_util.dsi_write_regs(addr, pdata, byte_nums)
+#define read_reg(cmd)											lcm_util.dsi_dcs_read_lcm_reg(cmd)
+#define read_reg_v2(cmd, buffer, buffer_size)   				lcm_util.dsi_dcs_read_lcm_reg_v2(cmd, buffer, buffer_size)
+
+static void lcm_set_util_funcs(const LCM_UTIL_FUNCS * util)
 {
-  memcpy(&g_LCM_UTIL_FUNCS, util, 0x58);
+	memcpy(&lcm_util, util, sizeof(LCM_UTIL_FUNCS));
 }
 
-void lcm_get_params_(LCM_PARAMS *params)
+static void lcm_get_params(LCM_PARAMS * params)
 {
-  LCM_PARAMS *params;
 
-  _memzero_(params, 0x2B0);
+  memset(params, 0, sizeof(LCM_PARAMS));
   params->dbi.te_edge_polarity = 0;
   params->dsi.vertical_sync_active = 3;
   params->dsi.vertical_backporch = 14;
@@ -28,81 +97,58 @@ void lcm_get_params_(LCM_PARAMS *params)
   params->dsi.horizontal_sync_active = 4;
 }
 
-void lcm_init_(void)
+static unsigned int lcm_compare_id(void)
 {
-  set_reset_pin(0);
-  mdelay(20);
-  set_reset_pin(1);
-  mdelay(20);
-  dsi_set_cmdq_V3();
+  return 1;
 }
 
-void lcm_suspend_(void)
+static void lcm_init(void)
 {
-  int buff[16]; 
+  SET_RESET_PIN(0);
+  MDELAY(20);
+  SET_RESET_PIN(1);
+  MDELAY(20);
+}
+
+static void lcm_suspend(void)
+{
+  int buff[16];
 
   buff[0] = 0x280500;
-  dsi_set_cmdq(buff, 1, 1);
+  dsi_set_cmdq(&buff, 1, 1);
   buff[0] = 0x100500;
-  dsi_set_cmdq(buff, 1, 1);
+  dsi_set_cmdq(&buff, 1, 1);
 }
 
-void lcm_resume_(void)
+static void lcm_resume(void)
 {
   int buff[16];
 
   buff[0] = 0x290500;
-  dsi_set_cmdq(buff, 1, 1);
+  dsi_set_cmdq(&buff, 1, 1);
   buff[0] = 0x110500;
-  dsi_set_cmdq(buff, 1, 1);
+  dsi_set_cmdq(&buff, 1, 1);
   printk("[KERNEL]------otm1283a_hd720_dsi_vdo_tcl----%s------\n", "lcm_resume");
 }
 
-void lcm_compare_id_(void)
+static void lcm_esd_recover(void)
 {
-  int byte;
-  char buff_2[4];
-  int buff[4];
-
-  set_reset_pin(1);
-  mdelay(5);
-  set_reset_pin(0);
-  mdelay(30);
-  set_reset_pin(1);
-  mdelay(50);
-  buff[0] = 0x53700;
-  dsi_set_cmdq(buff, 1, 1);
-  dsi_dcs_read_lcm_reg_v2(161, buff_2, 4);
-  byte = buff_2[3] | (buff_2[2] << 8);
-  printk("otm1283a kernel %s\n", "lcm_compare_id");
-  printk("%s, id = 0x%08x\n", "lcm_compare_id", byte);
-  dword_C0B54E0C = byte;
+  lcm_init();
+  lcm_resume();
 }
 
-void lcm_esd_check_(void)
+// ---------------------------------------------------------------------------
+//  Get LCM Driver Hooks
+// ---------------------------------------------------------------------------
+LCM_DRIVER otm1283a_hd720_dsi_vdo_tcl_lcm_drv =
 {
-  char buff_2[2];
-  int buff[4];
-
-  if ( dword_C0C5B398 )
-  {
-    dword_C0C5B398 = 0;
-  }
-  else
-  {
-    buff[0] = 0x13700;
-    dsi_set_cmdq(buff, 1, 1);
-    dsi_dcs_read_lcm_reg_v2(10, buff_2, 1);
-    if ( buff_2[0] != 0x9C )
-    {
-      printk("return true %s\n", "lcm_esd_check");
-    }
-    printk("return false %s\n", "lcm_esd_check");
-  }
-}
-
-void lcm_esd_recover_(void)
-{
-  lcm_init_();
-  lcm_resume_();
-}
+    .name			= "otm1283a_hd720_dsi_vdo_tcl",
+	.set_util_funcs = lcm_set_util_funcs,
+	.get_params     = lcm_get_params,
+	.init           = lcm_init,
+	.suspend        = lcm_suspend,
+	.resume         = lcm_resume,
+	.compare_id    = lcm_compare_id,
+	.esd_check   = lcm_esd_check,
+	.esd_recover   = lcm_esd_recover,
+};
